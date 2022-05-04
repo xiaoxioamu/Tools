b689431c92188e55f7ff8f3d83541a5dfa792592
xiaoxioamu <2553223427@qq.com>
2022-05-04 19:43:13 +0800

    automatically update:2022-05-04-19:43:13

--- Linux/git_auto_push.sh
+++ Linux/git_auto_push.sh
@@ -28,6 +28,6 @@ done < ${temFile}
 # Commit and push
 if [ $is_change == 1 ]
 then 
-	git commit -m "automatically update:"$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+	git commit -m "automatically update:"$(date +"%Y-%m-%d-%H:%M:%S")
 	git push
 fi
