1bcd9480d6d8177a6016c3137cf68ddd50726f2d
xiaoxioamu <2553223427@qq.com>
2022-05-05 22:01:01 +0800

    automatically update:📔2022-05-05-22:01:01

--- crop_dataset_image.py
+++ crop_dataset_image.py
@@ -118,6 +118,99 @@ class ImageProc:
 			if os.path.exists(label_path) and os.path.exists(img_path):
 				try:
 					img = cv2.imread(img_path)
+					boxes_coor = self.label2xywh(label_path)
+					if boxes_coor is not None: 
+						for box_coor in boxes_coor:
+							box_coor = [int(i) for i in box_coor]
+
+							xmin, ymin = (int(i) if i > 0 else 0 for i in (box_coor[0] - size / 2, box_coor[1] - size / 2))	
+							temp = box_coor[0] + size / 2, box_coor[1] + size / 2
+							xmax, ymax = (int(j) if j <= self.shape[i] else self.shape[i] for i, j in enumerate(temp))
+							box_coor_c = [xmin, ymin, xmax, ymax]
+
+							cropped_img = img[box_coor_c[1]:box_coor_c[3], box_coor_c[0]:box_coor_c[2]]
+							cv2.imwrite("test.jpg", cropped_img)
+				
+				except cv2.error:
+					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
+
+
+	def draw_boxes(self) -> None:
+
+		"""
+		Cropped image's object and save to specified directory.
+		"""
+
+		for label_path, _, img_path in self.get_label_img_path():
+			if os.path.exists(label_path) and os.path.exists(img_path):
+				try:
+					img = cv2.imread(img_path)
+					boxes_coor = self.label2xyxy(label_path)
+					if boxes_coor is not None:
+						for box_coor in boxes_coor:
+							box_coor = [int(i) for i in box_coor]
+							start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
+							boxed_image = cv2.rectangle(img, start_point, end_point, color=(0, 0, 255), thickness=2)	
+							cv2.imwrite("test.jpg", boxed_image)	
+
+				except cv2.error:
+					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
+
+
+	def update_label(self):
+		for label_path, _, img_path in self.get_label_img_path():
+			if os.path.exists(label_path) and os.path.exists(img_path):
+				try:
+					img = cv2.imread(img_path)
+					boxes_coor = self.label2xywh(label_path)
+					boxes_coor_c = []
+					if boxes_coor is not None: 
+						for box_coor in boxes_coor:
+							box_coor = [int(i) for i in box_coor]
+							boxes_coor_c.append(box_coor)
+						boxes_coor_c.sort(key=lambda x : x[0])
+				except cv2.error:
+					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
+
+
+	# For test
+	def detect_img_objects(self) -> None:
+
+		"""
+		Cropped image's object and save to specified directory.
+		"""
+
+		for label_path, _, img_path in self.get_label_img_path():
+			time.sleep(0.5)
+			if os.path.exists(label_path) and os.path.exists(img_path):
+				try:
+					img = cv2.imread(img_path)
+					boxes_coor = self.label2xyxy(label_path)
+					if boxes_coor is not None:
+						for box_coor in boxes_coor:
+							box_coor = [int(i) for i in box_coor]
+							# cropped_img = img[box_coor[1]:box_coor[3], box_coor[0]:box_coor[2]]
+							start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
+							boxed_image = cv2.rectangle(img, start_point, end_point, color=(0, 0, 255), thickness=2)
+							cv2.imwrite("test.jpg", boxed_image)
+				
+				except cv2.error:
+					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
+
+
+	def detect_spec_size_img(self, size: int) -> None:
+
+		"""
+		Crop image's object and save to specified directory.
+		
+		Args:
+			size (int): Specify the image size to crop
+		"""
+
+		for label_path, _, img_path in self.get_label_img_path():
+			if os.path.exists(label_path) and os.path.exists(img_path):
+				try:
+					img = cv2.imread(img_path)
 					# boxes_coor_xyxy = self.label2xyxy(label_path)
 					boxes_coor = self.label2xywh(label_path)
 					if boxes_coor is not None: 
@@ -147,54 +240,6 @@ class ImageProc:
 				except cv2.error:
 					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
 
-
-	def draw_boxes(self) -> None:
-
-		"""
-		Cropped image's object and save to specified directory.
-		"""
-
-		for label_path, _, img_path in self.get_label_img_path():
-			if os.path.exists(label_path) and os.path.exists(img_path):
-				try:
-					img = cv2.imread(img_path)
-					boxes_coor = self.label2xyxy(label_path)
-					if boxes_coor is not None:
-						for box_coor in boxes_coor:
-							box_coor = [int(i) for i in box_coor]
-							start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
-							boxed_image = cv2.rectangle(img, start_point, end_point, color=(0, 0, 255), thickness=2)	
-							cv2.imwrite("test.jpg", boxed_image)	
-
-				except cv2.error:
-					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
-
-
-# For test
-	def detect_img_objects(self) -> None:
-
-		"""
-		Cropped image's object and save to specified directory.
-		"""
-
-		for label_path, _, img_path in self.get_label_img_path():
-			time.sleep(0.4)
-			if os.path.exists(label_path) and os.path.exists(img_path):
-				try:
-					img = cv2.imread(img_path)
-					boxes_coor = self.label2xyxy(label_path)
-					if boxes_coor is not None:
-						for box_coor in boxes_coor:
-							box_coor = [int(i) for i in box_coor]
-							# cropped_img = img[box_coor[1]:box_coor[3], box_coor[0]:box_coor[2]]
-							start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
-							boxed_image = cv2.rectangle(img, start_point, end_point, color=(0, 0, 255), thickness=2)
-							cv2.imwrite("test.jpg", boxed_image)
-				
-				except cv2.error:
-					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
-
-
 if __name__ == "__main__":
 	label_tables_list = ["labels/test.txt", "labels/train.txt", "labels/other.txt"]
 	crop_size = 300
@@ -204,4 +249,5 @@ if __name__ == "__main__":
 		image_proc = ImageProc(label_table, image_shape)
 		# image_proc.crop_img_objects()
 		# image_proc.crop_spec_size_img(crop_size)
-		image_proc.detect_img_objects()
+		# image_proc.detect_img_objects()
+		image_proc.update_label()
