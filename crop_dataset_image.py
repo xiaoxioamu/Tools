5213ccc4481c1be2fe8e7e0f854e37da55a47a6c
xiaoxioamu <2553223427@qq.com>
2022-05-06 10:01:01 +0800

    automatically update:📔2022-05-06-10:01:01

--- crop_dataset_image.py
+++ crop_dataset_image.py
@@ -7,6 +7,7 @@ from draw_boxes import xyxy2xywh, xywhToxyxy
 
 
 class ImageProc:
+
 	"""
 	Crop image to needed size.
 
@@ -15,6 +16,7 @@ class ImageProc:
 		style (str): The label coordinate format
 		img_shape (tuple): Image shape (weight, height)
 	"""
+
 	def __init__(self, label_table: str, img_shape: tuple, style: str="xyxy"):
 		self.style = style
 		self.shape = img_shape
@@ -178,8 +180,8 @@ class ImageProc:
 							box_coor = [int(i) for i in box_coor]
 							boxes_coor_xyhw.append(box_coor)
 						boxes_coor_xyhw.sort(key=lambda x : x[0])
-						box_base = boxes_coor_xyhw[0]
 
+						box_base = boxes_coor_xyhw[0]
 						xmin, ymin = (int(i) if i > 0 else 0 for i in (box_base[0] - size / 2, box_base[1] - size / 2))	
 						temp = box_base[0] + size / 2, box_base[1] + size / 2
 						xmax, ymax = (int(j) if j <= self.shape[i] else self.shape[i] for i, j in enumerate(temp))
@@ -188,6 +190,7 @@ class ImageProc:
 
 						boxes_coor_xyhw_cr = []
 						boxes_coor_xyxy_cr = []
+						box_next_base = [] 
 						for box_coor in deepcopy(boxes_coor_xyhw):
 							box_coor[0] -= img_coor_bias[0]
 							box_coor[1] -= img_coor_bias[1]
@@ -200,10 +203,15 @@ class ImageProc:
 								cropped_img = img[img_base_xyxy[1]:img_base_xyxy[3], img_base_xyxy[0]:img_base_xyxy[2]]
 								start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
 								boxed_image = cv2.rectangle(cropped_img, start_point, end_point, color=(0, 0, 255), thickness=2)
-								cv2.imwrite("test.jpg", boxed_image)								
+								cv2.imwrite("test.jpg", boxed_image)
+
+							else: 	
+								box_next_base.append(box_coor) # box_next_base is next crop base
+
+
+
+
  
-
-
 				except cv2.error:
 					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
 
@@ -243,6 +251,7 @@ class ImageProc:
 		"""
 
 		for label_path, _, img_path in self.get_label_img_path():
+			time.sleep(0.2)
 			if os.path.exists(label_path) and os.path.exists(img_path):
 				try:
 					img = cv2.imread(img_path)
@@ -277,7 +286,7 @@ class ImageProc:
 
 if __name__ == "__main__":
 	label_tables_list = ["labels/test.txt", "labels/train.txt", "labels/other.txt"]
-	crop_size = 300
+	crop_size = 640
 	image_shape = (2048, 2048)
 	
 	for label_table in label_tables_list:
