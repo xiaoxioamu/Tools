7a7b2389c16cfbc2e7428fc716c384662e19aaa9
xiaoxioamu <2553223427@qq.com>
2022-05-06 12:01:01 +0800

    automatically update:📔2022-05-06-12:01:01

--- crop_dataset_image.py
+++ crop_dataset_image.py
@@ -169,7 +169,7 @@ class ImageProc:
 					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
 
 
-	def update_label_engine(self, boxes_coor_xyhw: list, img: numpy.ndarray) -> list :
+	def update_label_engine(self, boxes_coor_xyhw: list, img: numpy.ndarray):
 
 		"""
 		Update label engine, calculate label iteratively.
@@ -199,11 +199,10 @@ class ImageProc:
 			if box_coor[2] <= self.size and box_coor[3] <= self.size:
 				boxes_coor_xyhw.pop(0)
 				cropped_img = img[img_base_xyxy[1]:img_base_xyxy[3], img_base_xyxy[0]:img_base_xyxy[2]]
-				start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
-				boxed_image = cv2.rectangle(cropped_img, start_point, end_point, color=(0, 0, 255), thickness=2)
-				cv2.imwrite("test.jpg", boxed_image)
+				# start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
+				# boxed_image = cv2.rectangle(cropped_img, start_point, end_point, color=(0, 0, 255), thickness=2)
+				cv2.imwrite("test.jpg", cropped_img)
 
-			return boxes_coor_xyhw 
 
 
 	def update_label(self):
@@ -216,6 +215,7 @@ class ImageProc:
 		"""
 
 		for label_path, _, img_path in self.get_label_img_path():
+			time.sleep(1.0)
 			if os.path.exists(label_path) and os.path.exists(img_path):
 				try:
 					img = cv2.imread(img_path)
@@ -229,7 +229,7 @@ class ImageProc:
 
 					boxes_coor_xyhw_dc = deepcopy(boxes_coor_xyhw)
 					while boxes_coor_xyhw_dc:
-						self.update_label_engine(boxes_coor_xyhw, img)
+						self.update_label_engine(boxes_coor_xyhw_dc, img)
 
 				except cv2.error:
 					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
