8cffcbdb93759fe38e59cfd2def8e0132a98537f
xiaoxioamu <2553223427@qq.com>
2022-05-06 18:01:01 +0800

    🐬 update: 2022-05-06-18:01:01 🐬

--- crop_dataset_image.py
+++ crop_dataset_image.py
@@ -2,6 +2,7 @@ import cv2
 import os 
 import time
 import numpy
+import shutil
 from copy import deepcopy
 from rich.progress import track
 from draw_boxes import xyxy2xywh, xywhToxyxy
@@ -37,6 +38,9 @@ class ImageProc:
 
 		"""
 		Convert label txt annotations' coordinate to xywh and output image bounding box coordinates.
+
+		Args:
+			label_path (str): Label path
 		"""
 
 		boxes_coor = []
@@ -45,11 +49,12 @@ class ImageProc:
 				boxes = f.readlines()
 				for box in boxes:
 					box = box.strip().split(' ')
-					box.pop(0)
+					class_name = box.pop(0)
 					box = [float(i) for i in box]
 					if self.style == "xyxy":
 						box = xyxy2xywh(box)
 
+					box.insert(0, class_name)
 					boxes_coor.append(box)
 			return boxes_coor 
 
@@ -61,6 +66,9 @@ class ImageProc:
 
 		Args:
 			label_path (str): Label path
+		
+		Return:
+			list [['classname', xmin, ymin, xmax, ymax],]
 		"""
  
 		boxes_coor = []
@@ -69,11 +77,12 @@ class ImageProc:
 				boxes = f.readlines()
 				for box in boxes:
 					box = box.strip().split(' ')
-					box.pop(0)
+					class_name = box.pop(0)
 					box = [float(i) for i in box]
 					if self.style == "xywh":
 						box = xywhToxyxy(box)
 
+					box.insert(0, class_name)
 					boxes_coor.append(box)
 			return boxes_coor 
 
@@ -109,7 +118,7 @@ class ImageProc:
 					boxes_coor = self.label2xyxy(label_path)
 					if boxes_coor is not None:
 						for box_coor in boxes_coor:
-							box_coor = [int(i) for i in box_coor]
+							box_coor = [int(i) for i in box_coor[1:]]
 							cropped_img = img[box_coor[1]:box_coor[3], box_coor[0]:box_coor[2]]
 							cv2.imwrite("test.jpg", cropped_img)
 				
@@ -133,7 +142,7 @@ class ImageProc:
 					boxes_coor = self.label2xywh(label_path)
 					if boxes_coor is not None: 
 						for box_coor in boxes_coor:
-							box_coor = [int(i) for i in box_coor]
+							box_coor = [int(i) for i in box_coor[1:]]
 
 							xmin, ymin = (int(i) if i > 0 else 0 for i in (box_coor[0] - size / 2, box_coor[1] - size / 2))	
 							temp = box_coor[0] + size / 2, box_coor[1] + size / 2
@@ -160,7 +169,7 @@ class ImageProc:
 					boxes_coor = self.label2xyxy(label_path)
 					if boxes_coor is not None:
 						for box_coor in boxes_coor:
-							box_coor = [int(i) for i in box_coor]
+							box_coor = [int(i) for i in box_coor[1:]]
 							start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
 							boxed_image = cv2.rectangle(img, start_point, end_point, color=(0, 0, 255), thickness=2)	
 							cv2.imwrite("test.jpg", boxed_image)	
@@ -169,13 +178,60 @@ class ImageProc:
 					print(f"✈️✈️✈️✈️ cv2.error ✈️✈️✈️✈️	\nlabel_path: {label_path}\nimage_path: {img_path}\n")
 
 
-	def save_image_label(self):
+	def label_save(self, label_path: str, value: int) -> str:
 
 		"""
-		Save images and labels to specified path.
+		Save image to specified path.
+		Args:
+			label_path (str): Input image path
+			value (int): The size of image to crop
 		"""
 
-		
+		label_path_list = label_path.split('/')
+
+		crop_eq_value = f"cropped_" + str(value) 
+		label_new_dirs = os.path.join(label_path_list[0], crop_eq_value, label_path_list[1]) 
+		label_new_path = os.path.join(label_new_dirs, label_path_list[-1])
+		label_new_path_list = label_new_dirs.split('/')
+
+		temp_dir = ""
+		for i in label_new_path_list:
+			temp_dir += i
+			if not os.path.exists(temp_dir):
+				os.mkdir(temp_dir)
+			temp_dir += '/'
+
+		if not os.path.exists(label_new_path):
+			shutil.copyfile(label_path, label_new_path)		
+
+
+	def image_save(self, label_path: str, value: int, img: numpy.ndarray) -> str:
+
+		"""
+		Save image to specified path.
+		Args:
+			label_path (str): Input image path
+			value (int): The size of image to crop
+			img (numpy.ndarary): Image after cv2.imread
+		"""
+
+		img_path = label_path.replace("labels", "images").replace(".txt", ".jpg")
+		img_path_list = img_path.split('/')
+
+		crop_eq_value = f"cropped_" + str(value) 
+		img_new_dirs = os.path.join(img_path_list[0], crop_eq_value, img_path_list[1])
+		img_new_path = os.path.join(img_new_dirs, img_path_list[-1])
+		img_new_dirs_list = img_new_dirs.split('/')
+
+		temp_dir = ""
+		for i in img_new_dirs_list:
+			temp_dir += i
+			if not os.path.exists(temp_dir):
+				os.mkdir(temp_dir)
+			temp_dir += '/'
+		if not os.path.exists(img_new_path):
+			cv2.imwrite(img_new_path, img)
+
 
 	def update_label_engine(self, boxes_coor_xyhw: list, img: numpy.ndarray):
 
@@ -188,32 +244,32 @@ class ImageProc:
 		"""
 
 		box_base = boxes_coor_xyhw[0]
-		xmin, ymin = (int(i) if i > 0 else 0 for i in (box_base[0] - self.size / 2, box_base[1] - self.size / 2))	
-		temp = box_base[0] + self.size / 2, box_base[1] + self.size / 2
+		xmin, ymin = (int(i) if i > 0 else 0 for i in (box_base[1] - self.size / 2, box_base[2] - self.size / 2))	
+		temp = box_base[1] + self.size / 2, box_base[2] + self.size / 2
 		xmax, ymax = (int(j) if j <= self.shape[i] else self.shape[i] for i, j in enumerate(temp))
-		img_base_xyxy = [xmin, ymin, xmax, ymax]
-		img_coor_bias = img_base_xyxy[0], img_base_xyxy[1] 
+		img_base_xyxy = [box_base[0], xmin, ymin, xmax, ymax]
+		img_coor_bias = img_base_xyxy[1], img_base_xyxy[2] 
 
-		boxes_coor_xyhw_cr = []
+		# boxes_coor_xyhw_cr = []
 		boxes_coor_xyxy_cr = []
 
-		cropped_img = deepcopy(img[img_base_xyxy[1]:img_base_xyxy[3], img_base_xyxy[0]:img_base_xyxy[2]])
+		# cropped_img = deepcopy(img[img_base_xyxy[1]:img_base_xyxy[3], img_base_xyxy[0]:img_base_xyxy[2]])
 		
 		for box_coor in deepcopy(boxes_coor_xyhw):
+			if box_coor[3] > self.size or box_coor[4] > self.size:
+				return		
+
 			box_coor[0] -= img_coor_bias[0]
 			box_coor[1] -= img_coor_bias[1]
-			boxes_coor_xyhw_cr.append(box_coor)
+			# boxes_coor_xyhw_cr.append(box_coor)
 			box_coor = xywhToxyxy(box_coor)
 			box_coor = [int(i) for i in box_coor]
 			boxes_coor_xyxy_cr.append(box_coor)
 
-			if box_coor[2] > self.size or box_coor[3] > self.size:
-				return			
-
+			# start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
+			# boxed_image = cv2.rectangle(cropped_img, start_point, end_point, color=(0, 0, 255), thickness=2)
+			# cv2.imwrite("test.jpg", boxed_image)
 			boxes_coor_xyhw.pop(0)
-			start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
-			boxed_image = cv2.rectangle(cropped_img, start_point, end_point, color=(0, 0, 255), thickness=2)
-			cv2.imwrite("test.jpg", boxed_image)
 
 
 	def update_label(self):
@@ -226,7 +282,7 @@ class ImageProc:
 		"""
 
 		for label_path, _, img_path in self.get_label_img_path():
-			time.sleep(1.0)
+			# time.sleep(1.0)
 			if os.path.exists(label_path) and os.path.exists(img_path):
 				try:
 					img = cv2.imread(img_path)
@@ -234,9 +290,9 @@ class ImageProc:
 					boxes_coor_xyhw = []
 					if boxes_coor is not None: 
 						for box_coor in boxes_coor:
-							box_coor = [int(i) for i in box_coor]
+							box_coor = [int(i) if not isinstance(i, str) else i for i in box_coor]
 							boxes_coor_xyhw.append(box_coor)
-						boxes_coor_xyhw.sort(key=lambda x : x[0])
+						boxes_coor_xyhw.sort(key=lambda x : x[1])
 
 					boxes_coor_xyhw_dc = deepcopy(boxes_coor_xyhw)
 					while boxes_coor_xyhw_dc:
@@ -261,7 +317,7 @@ class ImageProc:
 					boxes_coor = self.label2xyxy(label_path)
 					if boxes_coor is not None:
 						for box_coor in boxes_coor:
-							box_coor = [int(i) for i in box_coor]
+							box_coor = [int(i) for i in box_coor[1:]]
 							# cropped_img = img[box_coor[1]:box_coor[3], box_coor[0]:box_coor[2]]
 							start_point, end_point = (box_coor[0], box_coor[1]), (box_coor[2], box_coor[3])
 							boxed_image = cv2.rectangle(img, start_point, end_point, color=(0, 0, 255), thickness=2)
@@ -285,7 +341,7 @@ class ImageProc:
 			if os.path.exists(label_path) and os.path.exists(img_path):
 				try:
 					img = cv2.imread(img_path)
-					boxes_coor = self.label2xywh(label_path)
+					boxes_coor = self.label2xywh(label_path[1:])
 					if boxes_coor is not None: 
 						for box_coor in boxes_coor:
 							time.sleep(0.2)
