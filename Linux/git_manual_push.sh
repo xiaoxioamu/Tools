cfef66a7e4fbbfe4980186ca4567e892ff6f3610
xiaoxioamu <2553223427@qq.com>
2022-05-06 14:49:54 +0800

    🦄  modified git_manual_push.sh  🦄

--- Linux/git_manual_push.sh
+++ Linux/git_manual_push.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
 <<com
-git push to github automatically
+git commit automatically
 com
 
-cd ~/Engineer/MyCodes/Tools
-git pull
-git add .
+cd ~/Engineer/MyCodes
+tree -d -L 1 -i > tempfile 
+sed -i '1d' tempfile
+sed -i '$d' tempfile
 
-is_change=0
-temFile=~/Engineer/MyCodes/temp/temFile_push
-# comments="automatically update: $(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+while read line; do 
+	if [ ${line} ]; then
+		cd ${line}
+		if [ -e .git ]; then 
+			is_change=0
+			temFile=~/Engineer/MyCodes/temp/temFile_commit
 
-# Determine if a file is exists, if file is not exists, then creat it.
-if [ ! -e ${temFile} ]; then 
-	touch ${temFile}
-fi 
+			# Determine if a file is exists, if file is not exists, then creat it.
+			if [ ! -e ${temFile} ]; then 
+				touch ${temFile}
+			fi 
+			git add .
+			git status > ${temFile}
 
-git status > ${temFile}
+			# Read temFile's information, if finds changed file, then sets is_change to 1
+			while read line1; do
+				if  test line1="Changes to be committed:"; then
+					is_change=1
+				fi
+			done < ${temFile}
 
-# Read temFile's information, if finds changed file, then sets is_change to 1
-while read line; do
- 	if  test line="Changes to be committed:"; then
-		is_change=1
-	fi
-done < ${temFile}
-
-# Commit and push
-if [ $is_change == 1 ]
-then 
-	git commit -m "🦄 $* 🦄"
-	git push
-fi
+			# Commit and push
+			if [ $is_change == 1 ]
+			echo "starting commit -- 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 --> "${line} 
+			then 
+				git commit -m "🦄  $*  🦄"
+				git push 
+			fi
+			echo -e "\n"
+		fi; cd ..
+	fi 
+done < tempfile 
